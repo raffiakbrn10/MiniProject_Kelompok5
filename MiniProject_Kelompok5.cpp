8b0681daa6e331759fd74ce4953d6274cd8519b9
Raffiakbarn <akbarraffi59@gmail.com>
2025-10-11 09:05:44 +0700

    Update MiniProject_Kelompok5.cpp

--- MiniProject_Kelompok5.cpp
+++ MiniProject_Kelompok5.cpp
@@ -3,20 +3,21 @@
 using namespace std;
 
 int main() {
-    // Variabel untuk pendaftaran dan data peserta
+    // Deklrasi variabel
     int total = 0;
     int anak = 0, remaja = 0, dewasa = 0;
     int pilih, usia;
-    char jenisKelamin;
+    char JenisKelamin;
     string nama;
 
-    // Variabel untuk peserta tertua dan termuda
-    int usiaTertua = 0, usiaTermuda = 0;
-    string namaTertua = "-", namaTermuda = "-";
+    // Deklarasi variabel untuk peserta tertua dan termuda
+    int UsiaTertua = 0, UsiaTermuda = 0;
+    string NamaTertua = "-", NamaTermuda = "-";
 
+    // Halaman menu utama
     while (true) {
         cout << "\n+===========================================+\n";
-        cout << "|    Pendaftaran Lomba Hari Kemerdekaan     |\n";
+        cout << "| PENDAFTARA LOMBA DIES NATALIS ITERA KE-11 |\n";
         cout << "+-------------------------------------------+\n";
         cout << "|                MENU UTAMA                 |\n";
         cout << "+-------------------------------------------+\n";
@@ -26,10 +27,11 @@ int main() {
         cout << "+===========================================+\n";
         cout << "Silahkan Pilih Menu: ";
         cin >> pilih;
-
+        // Validasi input pilihan menu, agar pilihan sesuai dengan menu yang ada
         if (cin.fail() || (pilih < 1 || pilih > 3)) {
             cin.clear();
             cin.ignore(1000, '\n');
+            // output yang keluar jika memilih opsi yang tidak sesuai menu
             cout << "\n+===========================================+\n";
             cout << "|        Maaf, Pilihan Tidak Valid!         |\n";
             cout << "|   Silahkan Masukkan Pilihan Sesuai Menu   |\n";
@@ -37,7 +39,8 @@ int main() {
             continue;
         }
         cin.ignore(1000, '\n');
-
+        
+        // Input data peserta
         if (pilih == 1) {
             cout << "\nSelamat Datang di Menu Pendaftaran Peserta\n";
             cout << "Masukkan Nama Peserta: ";
@@ -46,19 +49,19 @@ int main() {
             cin >> usia;
             cin.ignore(1000, '\n');
             cout << "Masukkan Jenis Kelamin (L/P): ";
-            cin >> jenisKelamin;
+            cin >> JenisKelamin;
             cin.ignore(1000, '\n');
 
-            // Hasil Output dari pendaftaran peserta
+            // Output yang akan keluar setelah pendaftaran peserta berhasil
             cout << "\n+===========================================+\n";
             cout << "|                DATA PESERTA               |\n";
             cout << "+-------------------------------------------+\n";
             cout << "| Nama          : " << setw(25) << left << nama << " |\n";
-            cout << "| Usia          : " << setw(25) << left <<usia << " |\n";
+            cout << "| Usia          : " << setw(25) << left << usia << " |\n";
             cout << "| Jenis Kelamin : ";
-                if (jenisKelamin == 'L' || 'l') {
+                if (JenisKelamin == 'L' || 'l') {
                     cout << setw(25) << left << "Laki-laki" << " |\n";
-                } else if (jenisKelamin == 'P' || 'p') {
+                } else if (JenisKelamin == 'P' || 'p') {
                     cout << setw(25) << left << "Perempuan" << " |\n";
                 } else {
                     cout << setw(25) << left << "Tidak Diketahui" << " |\n";
@@ -74,28 +77,28 @@ int main() {
                     cout << setw(25) << left << "Dewasa" << " |\n";
                 dewasa++;
                 }
-
             total++;
             cout << "+-------------------------------------------+\n";
             cout << "|            PENDAFTARAN BERHASIL!          |\n";
             cout << "+===========================================+\n";
         
 
-             // Logika untuk mencari peserta tertua dan termuda
+             // Logika untuk mencari peserta tertua dan termuda pada menu rekap
             if (total == 1) {
-                usiaTertua = usiaTermuda = usia;
-                namaTertua = namaTermuda = nama;
+                UsiaTertua = UsiaTermuda = usia;
+                NamaTertua = NamaTermuda = nama;
             } else {
-                if (usia > usiaTertua) {
-                usiaTertua = usia;
-                namaTertua = nama;
+                if (usia > UsiaTertua) {
+                UsiaTertua = usia;
+                NamaTertua = nama;
                 }
-                if (usia , usiaTermuda) {
-                usiaTermuda = usia;
-                namaTermuda = nama;
+                if (usia , UsiaTermuda) {
+                UsiaTermuda = usia;
+                NamaTermuda = nama;
                 }
             }
         }
+        // Output hasil rekap peserta
         else if (pilih == 2) {
             cout << "\n+===========================================+\n";
             cout << "|            REKAP PESERTA LOMBA            |\n";
@@ -105,11 +108,13 @@ int main() {
             cout << "| Dewasa          : " << setw(23) << left << dewasa << " |\n";
             cout << "| Total Peserta   : " << setw(23) << left << total << " |\n";
             cout << "+-------------------------------------------+\n";
-            cout << "| Peserta Tertua  : " << setw(15) << left << namaTertua << setw(2) << right << usiaTertua << " tahun |\n";
-            cout << "| Peserta termuda : " << setw(15) << left << namaTermuda << setw(2) << right << usiaTermuda << " tahun |\n";            cout << "+-------------------------------------------+\n";
+            cout << "| Peserta Tertua  : " << setw(15) << left << NamaTertua << setw(2) << right << UsiaTertua << " tahun |\n";
+            cout << "| Peserta termuda : " << setw(15) << left << NamaTermuda << setw(2) << right << UsiaTermuda << " tahun |\n";            
+            cout << "+-------------------------------------------+\n";
             cout << "|    TERIMAKASIH TELAH BERPARTISIPASI       |\n";
             cout << "+===========================================+\n";
 
+            // Ouput pada pilihan 3 jika keluar dari program
         } else if (pilih == 3) {
             cout << "\n+=============================================+\n";
             cout << "| Terima Kasih Telah Menggunakan Program Ini! |\n";
@@ -121,5 +126,4 @@ int main() {
         
     }
     return 0;
-
 }
